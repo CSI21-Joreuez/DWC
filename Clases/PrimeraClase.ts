e25a8d399ad421998693bc03c13c6b35a1df938d
CSI1-Joreuez <CSI1-Joreuez@colegioaltair.net>
2022-10-03 14:38:34 +0200

    Main

--- Clases/PrimeraClase.ts
+++ Clases/PrimeraClase.ts
@@ -1,21 +1,43 @@
 (() =>{
 class Heroe {
-    nombre: string;
-    raza: string ;
-    edad: number;
+    //!Puedes declarar las variables así
+    protected raza: string ;
+    private edad: number;
     esGuerrero: Boolean;
     fuerza: number;
-    constructor(nombre: string,raza: string,edad: number,esGuerrero: Boolean,fuerza: number) {
+    //! Tambien se puede declarar las variables en las porpiedades , como en el "nombre"
+    //!                  |       
+    //!                 |
+    constructor(public nombre: string,raza: string,edad: number,esGuerrero: Boolean,fuerza: number) {
         this.nombre = nombre;
         this.raza = raza;
         this.edad = edad;
         this.esGuerrero = esGuerrero;
         this.fuerza = fuerza;
     }
-
+public bio(): void
+{
+let mensaje: string = `${this.nombre} Guerrero: ${this.esGuerrero} su fuerza es: ${this.fuerza}`;
+console.log(mensaje);
 }
+
+public cambiarEstado(cambio: boolean): string{
+return this.cambiaGuerrero(cambio);
+}
+protected cambiaGuerrero(varlor:boolean): string{
+    if(varlor)
+    return "Ahora es un Guerrero.";
+    else
+        return "Ya no es un Guerrero.";
+    }
+}
+
 
 let aragorn = new Heroe('Aragorn','Numenoreano',87,true,50);
 
+console.log(aragorn.nombre);
 
+console.log(aragorn.cambiarEstado(true));
 })();
+
+
